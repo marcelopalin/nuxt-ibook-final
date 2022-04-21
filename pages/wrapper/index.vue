09ad89b46f033aca60e08fe9b25bc9c11c328904
Marcelo Facio Palin <marcelo.palin@gmail.com>
2022-04-21 07:07:20 -0300

    Adiciona pagina wrapper para testar as media queries

---
+++ pages/wrapper/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="wrapper">
+    <div class="box"></div>
+    <div class="box"></div>
+    <div class="box"></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.wrapper {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 200px;
+  grid-auto-flow: column;
+  grid-gap: 0.5rem;
+
+  @include screen('medium') {
+    grid-template-columns: 1fr 1fr;
+  }
+  @include screen('small') {
+    grid-template-columns: 1fr;
+  }
+
+  .box {
+    background-color: color('dark', 'darkest');
+  }
+}
+</style>
