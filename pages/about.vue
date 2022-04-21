e42d90b598b2f8271f2a646483f862f5023a3df8
Marcelo Facio Palin <marcelo.palin@gmail.com>
2022-04-21 07:35:36 -0300

    Adiciona pagina sobre

---
+++ pages/about.vue
@@ -0,0 +1,12 @@
+<template>
+  <About-template />
+  <!-- Página não deve ir Estilização! -->
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+  export default Vue.extend({
+    name: 'About',
+    layout: 'ibook'
+  })
+</script>
